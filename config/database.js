bb0b954e6c8ab0aa5900ba485861dbe0dcc43fad
Sofia <sonimavispc@gmail.com>
2024-06-24 14:03:39 +0300

    fixed connection to properties database

--- config/database.js
+++ config/database.js
@@ -3,20 +3,28 @@ import mongoose from 'mongoose';
 let connected = false;
 
 const connectDB = async () => {
- mongoose.set('strictQuery', true);
-  //if the database is already connected, don't connect again
+  mongoose.set('strictQuery', true);
+
+  // If the database is already connected, don't connect again
   if (connected) {
     console.log('MongoDB is already connected...');
     return;
   }
 
+  // Connect to MongoDB
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, 
+      socketTimeoutMS: 45000 
+    });
     connected = true;
     console.log('MongoDB connected...');
   } catch (error) {
-    console.error(error);
+    console.log('Error connecting to MongoDB:', error);
   }
 };
+
 
 export default connectDB;
